83b9285f1c24173904878fdeedaec9e5298b9fc2
mikihiroikura <miki.ut.sikepuri@gmail.com>
2020-07-22 03:09:28 +0900

    Debugのd.libファイルの出力

--- GetImgs_withLib/main.cpp
+++ GetImgs_withLib/main.cpp
@@ -1,7 +1,13 @@
 #include <HSC/KAYACoaXpressClass.hpp>
 
+#ifdef _DEBUG
+#define LIB_EXT "d.lib"
+#else
+#define LIB_EXT ".lib"
+#endif
 
-#pragma comment(lib,"KAYACoaXpressLib.lib")
+
+#pragma comment(lib,"KAYACoaXpressLib" LIB_EXT)
 
 int main() {
 	kayacoaxpress cam;
